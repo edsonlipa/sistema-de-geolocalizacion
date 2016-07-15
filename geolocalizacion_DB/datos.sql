7b4e304b1ac773cdc09dc81a6c24118596017961
edsonlipa <edson.lipa@gmail.com>
2016-07-15 02:54:00 -0500

    archivo de datos
    nuevos datos actualizaciones, eliminaciones... intenten tenerlo
    actualizado

---
+++ geolocalizacion_DB/datos.sql
@@ -0,0 +1,5 @@
+INSERT INTO `geolocalizacion`.`usuarios` (`id`, `nombres`, `usuarios`, `password`, `tipo`) 
+VALUES (NULL, 'Victor Lipa', 'victor123', '123456', 'estandar');
+-- id es nulo por que es autoinclmentable, traten 
+-- de ir llenado este archivo a medida que agregan datos 
+-- a la base de datos para sus pruebas
